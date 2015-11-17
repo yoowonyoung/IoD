d285089a7f0f8fd3bc9d950e7ae7e5d5ae3a0ced
Kim Kangyeon <Kim Kangyeon>
2015-11-17 09:58:20 +0900

    Module status tracking implemented

--- App/DoggyWare/app/src/main/java/iod/app/mobile/doggyware/MainActivity.java
+++ App/DoggyWare/app/src/main/java/iod/app/mobile/doggyware/MainActivity.java
@@ -1,10 +1,12 @@
 package iod.app.mobile.doggyware;
 
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,7 +16,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -31,7 +36,9 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.Random;
 
+import iod.app.mobile.model.ListData;
 import iod.app.mobile.model.ModuleBean;
 
 public class MainActivity extends AppCompatActivity
@@ -40,9 +47,7 @@ public class MainActivity extends AppCompatActivity
     HttpClient httpClient;
     HttpGet httpGet;
 
-    TextView tModuleStatus;
-
-    private String moduleStatusText = "";
+    ListView lModuleList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,16 +57,7 @@ public class MainActivity extends AppCompatActivity
         toolbar.setTitle("");
         setSupportActionBar(toolbar);
 
-        tModuleStatus = (TextView) findViewById(R.id.statusView);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        lModuleList = (ListView) findViewById(R.id.module_list);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -150,24 +146,108 @@ public class MainActivity extends AppCompatActivity
         final String response = httpClient.execute(httpGet, responseHandler);
 
         Gson gson = new Gson();
-        Type collectionType = new TypeToken<ArrayList<ModuleBean>>(){}.getType();
-        ArrayList<ModuleBean> moduleBeans = gson.fromJson(response, collectionType);
-
-        for(int i=0;i<moduleBeans.size();i++){
-            ModuleBean moduleBean = moduleBeans.get(i);
-            if(i==moduleBeans.size()-1) {
-                moduleStatusText += moduleBean.getModuleName() + " : " + moduleBean.getModuleStatus();
-            } else {
-                moduleStatusText += moduleBean.getModuleName() + " : " + moduleBean.getModuleStatus() + "\n";
-            }
-        }
+        Type collectionType = new TypeToken<ArrayList<ModuleBean>>() {
+        }.getType();
+        final ArrayList<ModuleBean> moduleBeans = gson.fromJson(response, collectionType);
 
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
+                ListViewAdapter adapter = new ListViewAdapter(MainActivity.this);
+                for (int i = 0; i < moduleBeans.size(); i++) {
+                    ModuleBean bean = moduleBeans.get(i);
+                    adapter.addItem(new ListData(bean.getModuleName(), bean.getModuleStatus()));
+                }
+                lModuleList.setAdapter(adapter);
                 dialog.dismiss();
-                tModuleStatus.setText(moduleStatusText);
             }
         });
+    }
+
+    private class ViewHolder {
+        public ImageView mModule;
+        public TextView mModuleNameAndStatus;
+    }
+
+    private class ListViewAdapter extends BaseAdapter {
+        private Context mContext = null;
+        private ArrayList<ListData> mListData = new ArrayList<ListData>();
+
+        public ListViewAdapter(Context mContext) {
+            super();
+            this.mContext = mContext;
+        }
+
+        @Override
+        public int getCount() {
+            return mListData.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mListData.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder;
+            if (convertView == null) {
+                holder = new ViewHolder();
+
+                LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                convertView = inflater.inflate(R.layout.module_status_list_item, null);
+
+                holder.mModule = (ImageView) convertView.findViewById(R.id.thumbnail);
+                holder.mModuleNameAndStatus = (TextView) convertView.findViewById(R.id.listSiteName);
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            ListData mData = mListData.get(position);
+            switch (mData.mModuleName) {
+                case "ENVN":
+                    if (mData.mModuleStatus.equals("FINE")) {
+                        holder.mModule.setImageResource(R.drawable.envn_fine);
+                        holder.mModuleNameAndStatus.setText("환경 모듈 : 정상");
+                    } else {
+                        holder.mModule.setImageResource(R.drawable.envn_dead);
+                        holder.mModuleNameAndStatus.setText("환경 모듈 : 작동 불가");
+                    }
+                    break;
+                case "MESR":
+                    if (mData.mModuleStatus.equals("FINE")) {
+                        holder.mModule.setImageResource(R.drawable.mesr_fine);
+                        holder.mModuleNameAndStatus.setText("측정 모듈 : 정상");
+                    } else {
+                        holder.mModule.setImageResource(R.drawable.mesr_dead);
+                        holder.mModuleNameAndStatus.setText("측정 모듈 : 작동 불가");
+                    }
+                    break;
+                case "FEED":
+                    if (mData.mModuleStatus.equals("FINE")) {
+                        holder.mModule.setImageResource(R.drawable.feed_fine);
+                        holder.mModuleNameAndStatus.setText("피딩 모듈 : 정상");
+                    } else {
+                        holder.mModule.setImageResource(R.drawable.feed_dead);
+                        holder.mModuleNameAndStatus.setText("피딩 모듈 : 작동 불가");
+                    }
+                    break;
+            }
+            return convertView;
+        }
+
+        public void addItem(ListData listData) {
+            mListData.add(listData);
+        }
+
+        public void removeAll() {
+            mListData.clear();
+        }
     }
 }
